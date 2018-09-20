669b0b2ea1d5a9419e80ad48224fdf7db60b7bf2
gianpietro <gianpietro.dimatteo@trinitysolucoes.com>
2018-09-20 16:31:47 -0300

    finished sservices tutorial

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   // Por enquanto so deliverando o heroes mockado
   // getHeroes(): Hero[] {
@@ -18,6 +19,8 @@ export class HeroService {
   // Retornando um observable do array de heróis
   // Observable<Hero[]> that emits a single value, the array of heroes
   getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
