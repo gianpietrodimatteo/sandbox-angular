d67fd3df07b7cce2f7229806e97182bdd9e3b7c4
gianpietro <gianpietro.dimatteo@trinitysolucoes.com>
2018-09-21 15:59:04 -0300

    Finished Tour Of Heroes Tutorial

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -30,4 +30,12 @@ export class HeroDetailComponent implements OnInit {
       .subscribe(hero => this.hero = hero);
   }
 
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
 }
